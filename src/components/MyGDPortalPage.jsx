97df47a14503f71de60dcf8be9eeb1a823a8a4d4
Scott MacLeod <scott.macleod1990@gmail.com>
2022-11-30 13:59:23 -0800

    uploading updates because I forgot to create a new branch

--- src/components/MyGDPortalPage.jsx
+++ src/components/MyGDPortalPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Logo from './pics/logo-main.jpeg'
 
 function MyGDPortalPage(props) {
     return (
         <div>
-            <div id="gd-logo">Logo</div>
+            <img src={Logo} id="gd-logo" />
             <Link to="faq" id="faq-link">FAQ</Link>
             <Link to="login" id="login">Login</Link>
             <Link to="signup" id="signup">Signup</Link>
@@ -12,12 +13,9 @@ function MyGDPortalPage(props) {
                 <h1>Global Deeds</h1>
                 <p>Your global opportunity search provider</p>
                 <p>Find reliable educational, employment and emergency resources here and rise to THE TOP!</p>
-                <div id="search-text">
-
                 <form>
-                    <input type="text"></input>
+                    <input id="search-text" type="text"></input>
                 </form>
-                </div>
             </div>
         </div>
     );
