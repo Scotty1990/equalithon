cbae2192134008d88fc555c9ad2d26152da80dda
Maham Khan <mahamimranamjad@gmail.com>
2022-12-06 21:32:33 -0800

    plan form

---
+++ src/components/Plans.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Plans(){
+return (
+    <div>
+        <h1>Plans</h1>
+        <h4>Please select the plan that fits you</h4>
+        <h5>A payment of $1.00 is required to continue, even the free option.</h5>
+    <form>
+        <label>Free</label>
+        <input type='checkbox' name='free'/>
+        <label>Bronze- Free Trail for 30 Days then $30/month or $270/year</label>
+        <input type='checkbox' name='bronze'/>
+        <label>Silver $49/month or $500/year</label>
+        <input type='checkbox' name='silver'/>
+        <label>Gold $65/month or $750/year</label>
+        <input type='checkbox' name='gold'/>
+        <label>Platinum $850/year</label>
+        <input type='checkbox' name='platinum'/>
+    </form>
+    </div>
+)
+}
+
+export default Plans
