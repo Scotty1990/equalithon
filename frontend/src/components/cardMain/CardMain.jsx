162b250a647827d5380f892652acf62b1cb743a2
fedechaves <federicochavesf@gmail.com>
2022-12-13 15:32:02 -0300

    feat: backend with profile - not finished

--- frontend/src/components/cardMain/CardMain.jsx
+++ frontend/src/components/cardMain/CardMain.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './cardMain.css'
 
 const CardMain = ({ name, job, about}) => {
--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { Link, useOutletContext } from "react-router-dom";
 import PostList from "../components/PostList";
 import { API_BASE } from "../constants";
+import ProfileMain from "../components/profileMain/ProfileMain"
+import SecondaryProfile from "../components/profileMain/SecondaryProfile";
 
 export function Profile() {
 	const { user, setMessages } = useOutletContext();
-
 	const [posts, setPosts] = useState([]);
+	const [name, setName] = useState('Your Name')
+	const [phone, setPhone] = useState('+5959 999 999')
+	const [email, setEmail] = useState('email@email.com')
+	const [address, setAddress] = useState('Asunción, Paraguay')
 
 	useEffect(() => {
 		fetch(API_BASE + "/api/profile", { credentials: "include" })
@@ -33,44 +38,15 @@ export function Profile() {
 	};
 
 	return (
-		<div className="container bg-sky-800">
+		<div className="container">
 			<div className="row mt-5">
 				<div className="col-6">
-					<div className="flex flex-col w-full">
-						<div className="mt-2 grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-  						<div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
-  						<div className="divider"></div> 
-  						<div className="mt-2 grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-						<div className="divider color-black"></div>
-						<div className="mt-2 grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
-  						<div className="divider"></div> 
-						<div className="mt-2 grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-					</div>
-					<div className="mt-5">
-						<h2>Add a post</h2>
-						<form action="/api/post/createPost" encType="multipart/form-data" method="POST" onSubmit={handleSubmit}>
-							<div className="mb-3">
-								<label htmlFor="title" className="form-label">Title</label>
-								<input type="text" className="form-control" id="title" name="title" />
-							</div>
-							<div className="mb-3">
-								<label htmlFor="caption" className="form-label">Caption</label>
-								<textarea className="form-control" id="caption" name="caption"></textarea>
-							</div>
-							<div className="mb-3">
-								<label htmlFor="imgUpload" className="form-label">Image</label>
-								<input type="file" className="form-control" id="imageUpload" name="file" />
-							</div>
-							<button type="submit" className="btn btn-primary" value="Upload">Submit</button>
-						</form>
-					</div>
+					<ProfileMain name={name} phone={phone} email={email} address={address}/>
 				</div>
 				<div className="col-6">
-					<PostList posts={posts} />
-					<div className="row justify-content-center mt-5">
-						<Link className="btn btn-primary" to="/feed">Return to Feed</Link>
-					</div>
+					<SecondaryProfile/>
 				</div>
+				
 			</div>
 		</div>
 	)
